3ff6e94770e58c9913ba07599e46c760f45c6f05
Cesar Lopez <cesarl@outlook.com>
2015-07-06 14:22:02 -0600

    WCF Basic and parms

---
+++ S1/TestService/IMyService.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+
+namespace TestService
+{
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        string GetData();
+
+        [OperationContract]
+        string GetMessage(string Name);
+
+        [OperationContract]
+        string GetResult(int sid, string sName, int M1, int M2, int M3);
+
+        [OperationContract]
+        int GetMax(int[] ar);
+
+        [OperationContract]
+        int[] GetSorted(int[] ar);
+    }
+}
---
+++ S1/TestService/MyService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TestService
+{
+    public class MyService: IMyService
+    {
+        public string GetData()
+        {
+            return "www.udemy.com";
+        }
+
+        public string GetMessage(string Name)
+        {
+            return "Hello Mr./Ms " + Name;
+        }
+
+        public string GetResult(int sid, string sName, int M1, int M2, int M3)
+        {
+            //Calculate average
+            double avg = (M1 + M2 + M3)/3.0;
+
+            if (avg < 35)
+                return "Fail";
+            else
+                return "Pass";
+
+        }
+
+        public int GetMax(int[] ar) 
+        {
+            int max = ar[0];
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] > max)
+                    max = ar[i];
+            }
+
+            return max;
+        }
+
+        public int[] GetSorted(int[] ar)
+        {
+            Array.Sort(ar);
+            return ar;
+        }
+    }
+}
