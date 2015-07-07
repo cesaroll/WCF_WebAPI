6839be2733b55405d69f9e6abf6c74424029ed12
Cesar Lopez <cesarl@outlook.com>
2015-07-07 08:55:46 -0600

    S2-L5: Hosting a WCF Service

---
+++ S1/MyServiceHost/Service.svc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using TestService;
+
+namespace MyServiceHost
+{
+    public class Service : MyService
+    {
+        
+    }
+}
