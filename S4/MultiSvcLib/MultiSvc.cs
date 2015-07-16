993f6d022703e7a6dc4ddcafee3d8e638d894813
Cesar Lopez <cesarl@outlook.com>
2015-07-15 20:39:50 -0600

    S4-L10 : End Points Implementation 1

---
+++ S4/MultiSvcLib/MultiSvc.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MultiSvcLib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class MultiSvc : IMultiSvc
+    {
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
